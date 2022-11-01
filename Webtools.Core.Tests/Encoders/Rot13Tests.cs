07ba4c1046ce56e77b83cfa0233da9a19f10c9ac
jan-at-rhea <112615243+jan-at-rhea@users.noreply.github.com>
2022-11-01 12:30:14 +0100

    mudblazor wasm UI

---
+++ Webtools.Core.Tests/Encoders/Rot13Tests.cs
@@ -0,0 +1,19 @@
+﻿using Webtools.Core.Encoders;
+
+namespace Webtools.Core.Tests.Encoders
+{
+    public class Rot13Tests
+    {
+        private const string Value1 = "There was a cute dog in 2008. (Zamborine)";
+        private const string Value2 = "Gurer jnf n phgr qbt va 2008. (Mnzobevar)";
+
+        [Theory]
+        [InlineData(Value1,Value2)]
+        [InlineData(Value2,Value1)]
+        public void Transform(string input, string expected)
+        {
+            var output = Rot13.Transform(input);
+            Assert.Equal(output, expected);
+        }
+    }
+}
---
+++ Webtools.Core/Encoders/Rot13.cs
@@ -0,0 +1,37 @@
+﻿namespace Webtools.Core.Encoders
+{
+    public static class Rot13
+    {
+        /// <summary>
+        /// Performs the ROT13 character rotation.
+        /// Taken from: https://thedeveloperblog.com/rot13
+        /// (refactored).
+        /// </summary>
+        public static string Transform(string value)
+        {
+            var array = value.ToCharArray();
+            for (var i = 0; i < array.Length; i++)
+            {
+                var number = (int)array[i];
+
+                switch (number)
+                {
+                    case >= 'a' and <= 'z' when number > 'm':
+                        number -= 13;
+                        break;
+                    case >= 'a' and <= 'z':
+                        number += 13;
+                        break;
+                    case >= 'A' and <= 'Z' when number > 'M':
+                        number -= 13;
+                        break;
+                    case >= 'A' and <= 'Z':
+                        number += 13;
+                        break;
+                }
+                array[i] = (char)number;
+            }
+            return new string(array);
+        }
+    }
+}
